f44f572874f2c782952dbba2e0c3731a28e93ac8
beera-1 <217328278+beera-1@users.noreply.github.com>
2025-07-06 00:13:22 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# tbl
+# ✈️ Depoly
+
+<details><summary>Deploy To Heroku ✨</summary>
+<p>
+<br>
+<a href="https://heroku.com/deploy?template=https://github.com/ruban9820/SCRAPPER-BOT">
+  <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy To Heroku">
+</a>
+</p>
+</details>
+
+<details><summary>Deploy To Render ✈️</summary>
+<p>
+<br>
+<br>
+<br>
+<a href="https://render.com/deploy?repo=https://github.com/ruban9820/SCRAPPER-BOT">
+<img src="https://render.com/images/deploy-to-render-button.svg" alt="Deploy to Render">
+</a>
+</details>
+
+<details><summary>Deploy To VPS 🪐</summary>
+<p>
+<pre>
+git clone https://github.com/ruban9820/SCRAPPER-BOT
+# Install Packages
+pip3 install -U -r requirements.txt
+Edit configs.py with variables as given below then run bot
+python3 bot.py
+</pre>
+</p>
+</details>
---
+++ configs.py
@@ -0,0 +1,68 @@
+from os import getenv as genv
+from pyrogram import Client
+
+API_ID = genv("API_ID", "")
+
+API_HASH = genv("API_HASH", "")
+
+BOT_TOKEN = genv("BOT_TOKEN", "")
+
+SUPPORT_GROUP = genv("SUPPORT_GROUP", "")
+
+UPDATES_CHANNEL = genv("UPDATES_CHANNEL", "")
+
+GROUP_ID = int(genv("GROUP_ID", ""))
+
+DATABASE_URL = genv(
+    "DATABASE_URL",
+    "",
+)
+
+USER_SESSION_STRING = genv(
+    "USER_SESSION_STRING",
+    "",
+)
+
+SERVER_URL = genv("SERVER_URL", "")
+
+
+RSS_CHAT = int(
+    genv("RSS_CHAT", "")
+)  # add the channel id where the torrent files need to be sent
+
+BASE_URL = genv(
+    "BASE_URL", "https://www.1tamilblasters.com"
+).lower()  # update the main domain if changed
+
+
+# change the theme as you required
+
+
+START_TXT = """<b>Hello {}, I am a Scrapper Bot!.
+๏ I can scrap links from 1Tamilblasters and update it in Bot..
+๏ Click on the help menu button below to get information about my commands.
+๏ Powered By @MadxBotz</b>"""
+
+
+HELP_TXT = """Send any Movie Name and I will provide torrent links.
+
+Available Commands
+
+-> /get - To Get Torrent Link of that Movie
+-> /list - To get last 10 Movie/Series details
+
+Updates - ⍟ @MadxBotz"""
+
+ABOUT_TXT = """<b>╔════❰ Madx Scrapper Bot ❱═══❍
+║ ┏━━━━━━━━━❥
+║ ┣ Mʏ ɴᴀᴍᴇ -> {}
+║ ┣ Uᴘᴅᴀᴛᴇꜱ -> <a href="https://MadxBotz">••Bᴏᴛs••</a>
+║ ┣ 𝖲ᴜᴘᴘᴏʀᴛ -> <a href="https://t.me/MadxBotzSupport"> Bᴏᴛs Sᴜᴩᴩᴏʀᴛ</a>
+║ ┣ ๏ Cʜᴇᴄᴋ ʜᴇʟᴘ ᴛᴏ ᴋɴᴏᴡ ᴍᴏʀᴇ.
+║ ┗━━━━━━━━━❥
+╚═════❰ @ ❱═════❍</b>"""
+
+
+WEEK_RELEASES_PATH = genv(
+    "RELEASES_PATH", "/index.php?/forums/topic/"
+)  # dont change this
