e2ac3c22408dd4ab8bc06bdb6210fb2fa4f4cdf2
beera-1 <217328278+beera-1@users.noreply.github.com>
2025-10-26 02:35:11 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -2,7 +2,6 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from configs import *
 from plugins.commands import user_pagination, show_document
-from db_instance import db
 
 @Client.on_callback_query()
 async def callback(bot, query):
@@ -79,5 +78,6 @@ async def callback(bot, query):
                 ]
             ),
         )
+
 
 
